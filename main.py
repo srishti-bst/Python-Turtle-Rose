7ef0e9f8276595deed6b8ca9c94f2cb2bcf23f6e
chirag-bst <97293124+chirag-bst@users.noreply.github.com>
2022-06-07 14:20:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    name = os.environ.get("NAME", "bluestacks")
+    name = os.environ.get("NAME", "Chirag")
     return "Hello {}!".format(name)
 
 
